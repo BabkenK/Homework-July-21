db0a8bf96f4da10a1c224bac61d28736754ca173
BabkenK <139691909+BabkenK@users.noreply.github.com>
2023-07-24 00:41:36 +0400

    Add files via upload

--- arrhomework.c
+++ arrhomework.c
@@ -6,16 +6,14 @@ int main () {
 	printf("Enter the size  of the array: ");
 	scanf("%d", &n);
 
-	int arr[n];
-	printf("Input %d elements (integer type) in the array: ", n);
-	for (i = 0, i < n; i++);
-       	{
-	scanf("%d", &arr [i]);
-	}
-	printf("Elements in the array are: ");
-	for (i = 0, i < n; i++);
-       	{
-		printf("%d", arr[i]);
+	char array[n];
+	printf("Enter  %d characters in the  array: ", n);
+	scanf("%s", array);
+       	}
+	printf("Numbers  in the array : ");
+	for (int i = 0, i < n; i++) {
+       	if (array [i] >= '0' && array [i] <= '9'){
+		printf("%c", array[i]);
 	}
 	return 0;
 }
